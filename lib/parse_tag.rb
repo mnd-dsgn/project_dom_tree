2dd65bc89d98042b335062a885731f520fd812d3
“Matthew <matthew.hinea@gmail.com>
2016-07-15 14:32:03 -0400

    restructures parse_tag method to return a Tag object

--- lib/parse_tag.rb
+++ lib/parse_tag.rb
@@ -2,12 +2,16 @@ P_TAG = "<p class='foo bar' id='baz' name='fozzie'>"
 DIV_TAG = "<div id = 'bim'>"
 IMG_TAG = "<img src='http://www.example.com' title='funny things'>"
 
+Tag = Struct.new(:type, :classes, :id, :name)
+
 def parse_tag(tag)
   # goal: capture any alphanumeric characters of any length between "<" " " 
-  type = tag.match(/<\w+ /).to_a[0][1..-1]
+  type = tag.match(/<\w+ /).to_a[0][1..-2]
   classes = tag.match(/(class=('|"))(.+?)(("|'))/).to_a[3]
   classes = classes.split(' ')
   id = tag.match(/(id=('|"))(\w+?)(("|'))/).to_a[3]
+  name = tag.match(/(name=('|"))(\w+?)(("|'))/).to_a[3]
+  Tag.new(type, classes, id, name)
 end
 
-print parse_tag(P_TAG)
+# print parse_tag(P_TAG)
