a0aa8d8cc14da72f3e84f0dee1041726e5a5d67f
“Matthew <matthew.hinea@gmail.com>
2016-07-15 14:25:30 -0400

    captured tag type, classes, and id - working on capturing further attributes

--- lib/parse_tag.rb
+++ lib/parse_tag.rb
@@ -2,6 +2,12 @@ P_TAG = "<p class='foo bar' id='baz' name='fozzie'>"
 DIV_TAG = "<div id = 'bim'>"
 IMG_TAG = "<img src='http://www.example.com' title='funny things'>"
 
-def parse_tag
-  #cha
+def parse_tag(tag)
+  # goal: capture any alphanumeric characters of any length between "<" " " 
+  type = tag.match(/<\w+ /).to_a[0][1..-1]
+  classes = tag.match(/(class=('|"))(.+?)(("|'))/).to_a[3]
+  classes = classes.split(' ')
+  id = tag.match(/(id=('|"))(\w+?)(("|'))/).to_a[3]
 end
+
+print parse_tag(P_TAG)
