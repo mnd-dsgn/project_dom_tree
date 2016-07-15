66081c9356ed97aa0e3b247412f068a1a80ea639
Hannah Squier <hannahsquier@gmail.com>
2016-07-15 15:56:32 -0700

    add more conditionals to parse script

--- lib/parse_tag.rb
+++ lib/parse_tag.rb
@@ -12,14 +12,16 @@ html_string = "<div>  div text before  <p>    p text  </p>  <div>    more div te
 
 
 class ScriptParser
-
+  attr_reader :root
   def initialize(html_string)
     @html = html_string
+    @root = Tag.new('page', nil, nil, nil, '', nil, [], 0)
+    parse_script
   end
 
   def parse_tag(tag)
     # goal: capture any alphanumeric characters of any length between "<" " " 
-    type = tag.match(/<\w+( |>)/).to_a[0][1..-2]
+    type = tag.match(/<\w+( |>)/).to_a[0][1..-2] if tag.match(/<\w+( |>)/).to_a[0]
     classes = tag.match(/(class=('|"))(.+?)(("|'))/).to_a[3]
     classes = classes.split(' ') if classes
     id = tag.match(/(id=('|"))(\w+?)(("|'))/).to_a[3]
@@ -28,7 +30,7 @@ class ScriptParser
     Tag.new(type, classes, id, name, '', nil, [], 0)
   end
 
-  def html_split
+  def parse_script
     # begin
     #loop through every character in html string
     # if < , create a child tag, connect it to current tag, set current tag to child tag 
@@ -39,38 +41,44 @@ class ScriptParser
 
 
 # "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
-
-    current_tag = parse_tag(html_string)
+  
+    current_tag = @root
     inside_opening_tag = true
     inside_closing_tag = false
-    html_string.each_char.with_index do |char, i|
-      
-      if char == "<" && current_tag.depth > 0
-        child_tag = parse_tag(html_string[i..-1])
+    
+    @html.each_char.with_index do |char, i|
+      if char == "<" 
+        child_tag = parse_tag(@html[i..-1])
         current_tag.children << child_tag
 
         child_tag.parent = current_tag
         current_tag = child_tag 
         current_tag.depth = current_tag.parent.depth + 1
         inside_opening_tag = true
+
       elsif inside_opening_tag && char == '>'
         inside_opening_tag = false
-      elsif char == "<" && html_string[i+1] = "/"
+
+
+      elsif char == "/" && @html[i-1] == '<'
+        p current_tag.type
+        p "hi"
         current_tag = current_tag.parent
         inside_closing_tag = true
+        p current_tag.type
+        
       elsif inside_closing_tag && char == '>'
         inside_closing_tag = false
+       
+
       elsif inside_closing_tag == false && inside_opening_tag == false
         current_tag.inside << char 
       end
 
     end
-    current_tag
+    @root
   end
 
-  def parse_script
-    @root = parse_tag()
-  end
 end
 
 # Finally, output the string again.
@@ -97,6 +105,9 @@ end
 #parent has child if new tag opens before parent closes
 #parent's inside is when parent is the only opened tag and there is text
 # 
-sp = ScriptParser.new(html_string)
-print sp.parse_tag(html_string)
+sp = ScriptParser.new(html_string).root
+
+sp.children.each do |child|
+  puts child
+end
 
