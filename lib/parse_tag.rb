2ddc0f434b641e36280bbbeecab4425de86976df
Hannah Squier <hannahsquier@gmail.com>
2016-07-15 14:54:19 -0700

    add html_split method

--- lib/parse_tag.rb
+++ lib/parse_tag.rb
@@ -2,32 +2,30 @@ P_TAG = "<p class='foo bar' id='baz' name='fozzie'>"
 DIV_TAG = "<div id = 'bim'>"
 IMG_TAG = "<img src='http://www.example.com' title='funny things'>"
 
-Tag = Struct.new(:type, :classes, :id, :name, :inside, :parent, :children)
+Tag = Struct.new(:type, :classes, :id, :name, :inside, :parent, :children, :depth)
 
-def parse_tag(tag)
-  # goal: capture any alphanumeric characters of any length between "<" " " 
-  type = tag.match(/<\w+ /).to_a[0][1..-2]
-  classes = tag.match(/(class=('|"))(.+?)(("|'))/).to_a[3]
-  classes = classes.split(' ')
-  id = tag.match(/(id=('|"))(\w+?)(("|'))/).to_a[3]
-  name = tag.match(/(name=('|"))(\w+?)(("|'))/).to_a[3]
-  inside = # for tags that require, any content 
-  Tag.new(type, classes, id, name)
-end
 
-# The HTML string version
-# You could read it in from a file instead if so inclined,
-#   which would give you newline characters too
 html_string = "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
 
 
 # <(div)>(.+)<\/\1>
 
-# Now pull that string into a simple data structure
+
 class ScriptParser
 
   def initialize(html_string)
     @html = html_string
+  end
+
+  def parse_tag(tag)
+    # goal: capture any alphanumeric characters of any length between "<" " " 
+    type = tag.match(/<\w+( |>)/).to_a[0][1..-2]
+    classes = tag.match(/(class=('|"))(.+?)(("|'))/).to_a[3]
+    classes = classes.split(' ') if classes
+    id = tag.match(/(id=('|"))(\w+?)(("|'))/).to_a[3]
+    name = tag.match(/(name=('|"))(\w+?)(("|'))/).to_a[3]
+    inside = # for tags that require, any content 
+    Tag.new(type, classes, id, name, nil, nil, [], 0)
   end
 
   def html_split
@@ -38,6 +36,28 @@ class ScriptParser
     # any text found here is added to "inside" data of current tag
       # if </ set current tag to current tag's parent
       # end until current tag. parent is nil
+
+
+# "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
+
+    current_tag = parse_tag(html_string)
+    
+    html_string.each_char.with_index do |char, i|
+      
+      if char == "<" && current_tag.depth > 0
+        child_tag = parse_tag(html_string[i..-1])
+        current_tag.children << child_tag
+
+        child_tag.parent = current_tag
+        current_tag = child_tag 
+        current_tag.depth = current_tag.parent.depth + 1
+
+      elsif char == "<" && html_string[i+1] = "/"
+        current_tag = current_tag.parent
+      end
+
+    end
+    current_tag
   end
 
   def parse_script
@@ -47,19 +67,19 @@ end
 
 # Finally, output the string again.
 # It doesn't have to have pretty spacing like it does here...
-outputter( data_structure )
+# outputter( data_structure )
 
-<div>
-  div text before
-  <p>  # < signifies begin child tag creation
-      # </ signifies end child tag creation
-    p text
-  </p>
-  <div>
-    more div text
-  </div>
-  div text after # if not in a child tag, any text is added to :inside
-</div>
+# <div>
+#   div text before
+#   <p>  # < signifies begin child tag creation
+#       # </ signifies end child tag creation
+#     p text
+#   </p>
+#   <div>
+#     more div text
+#   </div>
+#   div text after # if not in a child tag, any text is added to :inside
+# </div>
 
 # as we iterate, keep track of:
 #tag we are inside of
@@ -69,5 +89,6 @@ outputter( data_structure )
 #parent has child if new tag opens before parent closes
 #parent's inside is when parent is the only opened tag and there is text
 # 
+sp = ScriptParser.new(html_string)
+print sp.parse_tag(html_string)
 
-# print parse_tag(P_TAG)
